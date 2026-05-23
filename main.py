072c564c90064e7fa953553dc573643b4d8f8969
Ирина Сергеевна Смирнова <35097671+disath1554@users.noreply.github.com>
2026-05-23 12:15:58 +0300

    Update main.py
    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, request
 from flask_login import LoginManager, login_user, login_required, logout_user
 
 from data import db_session
@@ -92,6 +92,42 @@ def addjob():
         db_sess.commit()
         return redirect('/')
     return render_template('addjob.html', title='Adding a job', form=add_form)
+
+
+@app.route('/jobs/<int:id>', methods=['GET', 'POST'])
+@login_required
+def job_edit(id):
+    form = AddJobForm()
+    if request.method == "GET":
+        db_sess = db_session.create_session()
+        jobs = db_sess.query(Jobs).filter(Jobs.id == id,
+                                          (Jobs.team_leader == current_user.id) | (
+                                                      current_user.id == 1)).first()
+        if jobs:
+            form.job.data = jobs.job
+            form.team_leader.data = jobs.team_leader
+            form.work_size.data = jobs.work_size
+            form.collaborators.data = jobs.collaborators
+            form.is_finished.data = jobs.is_finished
+        else:
+            abort(404)
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        jobs = db_sess.query(Jobs).filter(Jobs.id == id,
+                                          (Jobs.team_leader == current_user.id) | (
+                                                      current_user.id == 1)).first()
+        if jobs:
+            jobs.job = form.job.data
+            jobs.team_leader = form.team_leader.data
+            jobs.work_size = form.work_size.data
+            jobs.collaborators = form.collaborators.data
+            jobs.is_finished = form.is_finished.data
+            db_sess.commit()
+            return redirect('/')
+        else:
+            abort(404)
+    return render_template('addjob.html', title='Job Edit', form=form)
+
 
 
 #http://127.0.0.1:5000/login
